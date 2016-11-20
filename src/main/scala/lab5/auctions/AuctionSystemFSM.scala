e876103ee0ecd7f6db7bb5e7c83aa7dea08d07b4
Malgorzata Salawa <mksalawa@gmail.com>
2016-11-20 15:59:09 +0100

    Lab5 - copy lab4

---
+++ src/main/scala/lab5/auctions/AuctionSystemFSM.scala
@@ -0,0 +1,83 @@
+package lab5.auctions
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.event.LoggingReceive
+import lab5.auctions.actors.{AuctionSearch, Buyer, Seller}
+
+import scala.collection.immutable.HashMap
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+
+object AuctionSystem {
+  case object Init
+}
+
+class AuctionSystem() extends Actor {
+
+  import AuctionSystem._
+
+  private val sellersMap = HashMap[String, List[String]](
+//    "seller1" -> List("Neksus 6 new phone")
+
+    "seller1" -> List("Neksus 6 new phone", "Phone case Sansung S34", "MyPhone 7 best price"),
+    "seller2" -> List("Proshe 911 2012 350hp", "Gaudi G6 2014 eco", "Thesla 5 2015 electric"),
+    "seller3" -> List("Bed sheets floral, various colours", "Pillow 30x30, soft & fluffy")
+  )
+
+  private val buyersMap = HashMap[Int, List[String]](
+//    0 -> List("Neksus", "Pillow")
+
+    0 -> List("Neksus", "Pillow"),
+    1 -> List("Proshe", "Neksus", "Gaudi"),
+    2 -> List("Proshe", "Pillow", "Bed"),
+    3 -> List("Sansung", "Thesla", "Gaudi"),
+    4 -> List("Sansung", "MyPhone", "Thesla")
+  )
+
+  val search = context.actorOf(Props[AuctionSearch], "search")
+
+  val buyers: ArrayBuffer[String] = {
+    val bs = new ArrayBuffer[String]()
+    buyersMap.foreach { case (buyerId, queries) =>
+      val b = context.actorOf(Buyer.props(search.path.toStringWithoutAddress, 50), s"buyer$buyerId")
+      bs += b.path.toStringWithoutAddress
+    }
+    bs
+  }
+
+  val sellers: ArrayBuffer[String] = {
+    val ss = new ArrayBuffer[String]()
+    sellersMap.foreach { case (seller, auctions) =>
+      val s = context.actorOf(Seller.props(search.path.toStringWithoutAddress, 10 seconds, 10 seconds), seller)
+      s ! Seller.CreateAuctions(auctions)
+      ss += s.path.toStringWithoutAddress
+    }
+    ss
+  }
+
+  def receive = LoggingReceive {
+    case Init =>
+      // we need to wait for the auctions to be created...
+      Thread sleep 1000
+      buyersMap.foreach { case (buyerId, queries) =>
+        context.actorSelection(buyers(buyerId)) ! Buyer.Buy(queries)
+        Thread sleep 50
+      }
+    case Seller.SellerFinished =>
+      println(s"SELLER FINISHED | ${sender.path.name}")
+      sellers -= sender.path.toStringWithoutAddress
+      if (sellers.isEmpty) {
+        println(s"ALL AUCTIONS FINISHED | Terminating system...")
+        context.system.terminate()
+      }
+  }
+}
+
+object AuctionSystemApp extends App {
+  val system = ActorSystem("AuctionSystem")
+  val mainActor = system.actorOf(Props[AuctionSystem], "mainActor")
+  mainActor ! AuctionSystem.Init
+  Await.result(system.whenTerminated, Duration.Inf)
+}
---
+++ src/main/scala/lab5/auctions/actors/Auction.scala
@@ -0,0 +1,134 @@
+package lab5.auctions.actors
+
+import akka.actor.{ActorRef, Props}
+import akka.persistence.fsm.PersistentFSM.FSMState
+import akka.persistence.fsm._
+import lab5.auctions.actors.Auction.StateChangeEvent
+
+import scala.concurrent.duration.{FiniteDuration, _}
+import scala.reflect._
+
+
+object Auction {
+  case class BidSuccess(auction: ActorRef)
+  case class BidFailed(auction: ActorRef)
+  case class BidRaised(auction: ActorRef)
+  case class Win(auction: ActorRef)
+  case class AuctionExpired(auctionData: AuctionData)
+  case class Subscribe(listener: ActorRef)
+  case class Bid(amount: BigInt, buyer: ActorRef) {
+    require(amount > 0)
+  }
+  case class StateChangeEvent(data: AuctionData)
+
+  def props(id: String, title: String, bidTime: FiniteDuration, deleteTime: FiniteDuration): Props =
+    Props(new Auction(id, title, bidTime, deleteTime))
+}
+
+class Auction(id: String, title: String, bidTime: FiniteDuration, deleteTime: FiniteDuration) extends PersistentFSM [State, AuctionData, StateChangeEvent] {
+  import lab5.auctions.actors.Auction._
+
+  case object ClockTick
+
+  override def persistenceId = id
+  override def domainEventClassTag: ClassTag[StateChangeEvent] = classTag[StateChangeEvent]
+
+  private val TICK: FiniteDuration = (1000 millis)
+
+  startWith(Created, AuctionData(title, 0, null, bidTime))
+  setTimer("clock", ClockTick, TICK, repeat = true)
+
+  whenUnhandled {
+    case Event(ClockTick, AuctionData(t, currAmount, currBuyer, timeout)) =>
+//      println(s"=> TICK | $title")
+      val newTimeLeft: FiniteDuration = timeout - TICK
+      if (newTimeLeft <= (0 millis)) {
+        self ! StateTimeout
+        stay applying StateChangeEvent(AuctionData(t, currAmount, currBuyer, deleteTime))
+      } else {
+        stay applying StateChangeEvent(AuctionData(t, currAmount, currBuyer, newTimeLeft))
+      }
+  }
+
+  when(Created, stateTimeout = bidTime) {
+    case Event(Bid(amount, buyer), _) =>
+      println(s"BID OK! | $title | [state: Created] ${buyer.path.name} | $amount")
+      buyer ! Auction.BidSuccess(self)
+      goto(Activated) applying StateChangeEvent(AuctionData(title, amount, buyer.path.toStringWithoutAddress, bidTime))
+    case Event(StateTimeout, _) =>
+      println(s"BID TIMEOUT! | $title | [state: Created]")
+      goto(Ignored)
+  }
+
+  when(Ignored, stateTimeout = deleteTime) {
+    case Event(Bid(amount, buyer), _) =>
+      println(s"BID OK! | $title | [state: Ignored] ${buyer.path.name} | $amount")
+      buyer ! Auction.BidSuccess(self)
+      goto(Activated) applying StateChangeEvent(AuctionData(title, amount, buyer.path.toStringWithoutAddress, bidTime))
+    case Event(StateTimeout, data) =>
+      println(s"DELETE TIMEOUT! | $title | [state: Ignored]")
+      context.parent ! AuctionExpired(data)
+      goto(Expired)
+  }
+
+  when(Activated, stateTimeout = bidTime) {
+    case Event(Bid(amount, buyer), AuctionData(t, currAmount, currBuyer, timeout)) if amount > currAmount =>
+      println(s"BID OK! | $title | [state: Activated] ${buyer.path.name} | $amount")
+      buyer ! Auction.BidSuccess(self)
+      context.actorSelection(currBuyer) ! BidRaised(self)
+      stay applying StateChangeEvent(AuctionData(t, amount, buyer.path.toStringWithoutAddress, bidTime))
+    case Event(Bid(amount, buyer), AuctionData(t, currAmount, currBuyer, timeout)) =>
+      println(s"BID TOO LOW! | $title | [state: Activated] ${buyer.path.name} | $amount")
+      buyer ! Auction.BidFailed(self)
+      stay
+    case Event(StateTimeout, AuctionData(t, currAmount, currBuyer, timeout)) =>
+      println(s"BID TIMEOUT! | $title | [state: Activated]")
+      goto(Sold) applying StateChangeEvent(AuctionData(t, currAmount, currBuyer, bidTime))
+  }
+
+  when(Sold, stateTimeout = deleteTime) {
+    case Event(StateTimeout, data) =>
+      println(s"DELETE TIMEOUT! | $title | [state: Sold]")
+      context.parent ! data
+      goto(Expired)
+  }
+
+  when(Expired) {
+    case _ => stay
+  }
+
+  override def onRecoveryCompleted(): Unit = super.onRecoveryCompleted()
+
+  override def applyEvent(event: StateChangeEvent, dataBeforeEvent: AuctionData): AuctionData = {
+    val data = event.data
+    var buyer = "null"
+    if (dataBeforeEvent.currentBuyer != null) {
+      buyer = dataBeforeEvent.currentBuyer
+    }
+    println(s"CHANGE $title \n\tFROM: ${dataBeforeEvent.currentBid} | $buyer | ${dataBeforeEvent.timeout} \n\tTO: " +
+      s"${data.currentBid} | ${data.currentBuyer} | ${data.timeout}")
+    data
+  }
+}
+
+// States of FSM
+sealed trait State extends FSMState
+case object Created extends State {
+  override def identifier: String = "Created"
+}
+case object Ignored extends State {
+  override def identifier: String = "Ignored"
+}
+case object Activated extends State {
+  override def identifier: String = "Activated"
+}
+case object Sold extends State {
+  override def identifier: String = "Sold"
+}
+case object Expired extends State {
+  override def identifier: String = "Expired"
+}
+
+// Data that may be retained within FSM
+sealed trait Data
+case class AuctionData(title: String, currentBid: BigInt, currentBuyer: String, timeout: FiniteDuration) extends Data
